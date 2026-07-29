ea38271700bf25703213c743c74acef6635b560f
Juninho Alves <junioralv290302@gmail.com>
2026-07-29 14:06:00 -0300

    ajustes

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -6,14 +6,39 @@ import { Label } from "../Label";
 import { Botao } from "../Botao";
 import { ListaSuspensa } from "../ListaSuspensa";
 
-export function FormularioDeEvento() {
+export function FormularioDeEvento({ temas }) {
+  function aoFormSubmetido(formData) {
+    console.log("opa, tá na hora de criar um novo evento", formData);
+    const evento = {
+      capa: formData.get("capa "),
+      tema: temas.find(function (item) {
+        return item.id == formData.get("tema");
+      }),
+      data: new Date(formData.get("dataEvento")),
+      titulo: formData.get("nomeEvento"),
+    };
+    console.log("esse é o evento", evento);
+  }
+
   return (
-    <form className="form-evento">
+    <form className="form-evento" action={aoFormSubmetido}>
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
       <div className="campos">
         <CampoDeFormulario>
           <Label htmlFor="nomeEvento">Qual é o nome do evento?</Label>
           <CampoDeDados type="text" id="nomeEvento" name="nomeEvento" />
+        </CampoDeFormulario>
+
+        <CampoDeFormulario>
+          <Label htmlFor="capaEvento">
+            Qual é o endereço da imagem de capa?
+          </Label>
+          <CampoDeDados
+            type="text"
+            id="capaEvento"
+            placeholder="http://..."
+            name="nomeEvento"
+          />
         </CampoDeFormulario>
 
         <CampoDeFormulario>
@@ -27,8 +52,8 @@ export function FormularioDeEvento() {
         </CampoDeFormulario>
 
         <CampoDeFormulario>
-          <Label htmlFor="dataEvento">Data do evento:</Label>
-          <ListaSuspensa />
+          <Label htmlFor="temaEvento">Tema do evento:</Label>
+          <ListaSuspensa id="temaEvento" name="tema" itens={temas} />
         </CampoDeFormulario>
       </div>
       <div className="acoes">
